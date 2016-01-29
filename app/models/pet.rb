31a05419b1d0cbefbe139e0d10b96a4698704630
Richard Boldway <richard.boldway@gmail.com>
2016-01-29 08:33:32 -0600

    Change the age and birthday relationship handling to use callbacks.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,19 +1,21 @@
 class Pet < ActiveRecord::Base
  
+  after_find :age_per_birthday
+  before_update :birthday_per_age
+
   attr_accessor :age
   
   validates :name, length: {minimum: 1, maximum: 35}
   validates :breed, length: {minimum: 1, maximum: 35}
   validates :age, presence: true
   validates :weight, presence: true
-  # validates_associated :visits
-  
-  def age
-    self.birthday = Date.today - @age.to_i  
+
+  def birthday_per_age
+    self.birthday = Date.today - @age.to_i
   end
-  
+
   def age_per_birthday
-    self.birthday ? (Date.today - self.birthday).round : 0
+    self.age = self.birthday ? (Date.today - self.birthday).round : 0
   end  
   
 end
